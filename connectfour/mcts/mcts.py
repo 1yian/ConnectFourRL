bb22c4d094971ed6165f8212914bf9114af45d33
yian <yian@utexas.edu>
2020-06-14 21:47:48 -0500

    finished up a2c code, moving on to mcts.

---
+++ connectfour/mcts/mcts.py
@@ -0,0 +1,15 @@
+class MCTSNode:
+    def __init__(self, state):
+        self.state = state
+
+    def select(self, action):
+        pass
+
+    def get_possible_actions(self):
+        pass
+
+    def is_terminal(self):
+        pass
+
+    def get_reward(self):
+        pass
